e66b03f41d98361403caa4d595c6ef338d6b6e61
santanu619 <mohapatra.santanu123@gmail.com>
2021-04-20 18:02:44 +0530

    Added: Code of Factorial using Recursion

---
+++ Factorial_Recursion/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Factorial_Recursion
+{
+    class Program
+    {
+
+            static void Main(string[] args)
+
+            {
+
+                Console.WriteLine("Enter a Number");
+
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                long fact = FindFactorial(num);
+
+                Console.WriteLine("{0} factorial is {1}", num, fact);
+
+                Console.ReadKey();
+
+            }
+
+
+
+            private static long FindFactorial(int number)
+
+            {
+
+                if (number == 0)
+
+                {
+
+                    return 1;
+
+                }
+
+                return number * FindFactorial(number - 1);
+
+            }
+
+        }
+
+    }
